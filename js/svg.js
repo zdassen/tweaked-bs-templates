54db0353bcb8f4bd217ff434936182794bb3e50a
zdassen <e3eas9lt1wuyifw@yahoo.co.jp>
2018-07-18 10:39:05 +0900

    svg sizer modify

--- js/svg.js
+++ js/svg.js
@@ -4,40 +4,42 @@
  *
  */
 
- 
-/* スクロールバーを非表示にする */
-function hiddenScrollbar() {
 
-  // body 要素を取得
-  let b = document.querySelector("body");
-
-  // スクロールバーを非表示にする
-  b.style.overflow = "hidden";
-  b.style.overflowY = "hidden";
-
-}
+const SVG = (function () {
 
 
-/* SVG 要素のサイズを画面一杯に設定する */
-function setupFullSize(svgId) {
+  /* スクロールバーを非表示にする */
+  function hiddenScrollbar() {
 
-  // スクロールバーを非表示にする
-  hiddenScrollbar();
+    // body 要素を取得
+    let b = document.querySelector("body");
 
-  // SVG 要素を取得
-  let svg = document.getElementById(svgId);
+    // スクロールバーを非表示にする
+    b.style.overflow = "hidden";
+    b.style.overflowY = "hidden";
 
-  // サイズを設定する
-  let w = window.innerWidth;
-  let h = window.innerHeight;
-  svg.setAttribute("viewBox", `0 0 ${w} ${h}`);
-
-  return svg;
-}
+  }
 
 
-(function () {
+  /* SVG 要素のサイズを画面一杯に設定する */
+  function setupFullSize(svgId) {
 
-  let svg = setupFullSize("graphArea");
+    // スクロールバーを非表示にする
+    hiddenScrollbar();
 
+    // SVG 要素を取得
+    let svg = document.getElementById(svgId);
+
+    // サイズを設定する
+    let w = window.innerWidth;
+    let h = window.innerHeight;
+    svg.setAttribute("viewBox", `0 0 ${w} ${h}`);
+
+    return svg;
+  }
+
+
+  return {
+    setupFullSize: setupFullSize,
+  };
 }());
---
+++ navbar/main.js
@@ -0,0 +1,15 @@
+/**
+ *
+ * main.js
+ *
+ */
+(function () {
+
+  /*
+   * SVG 要素のサイズを画面いっぱいに設定する
+   * & スクロールバーを非表示にする
+   */
+  let svgId = "graphArea";
+  let svg = SVG.setupFullSize(svgId);
+
+}());
